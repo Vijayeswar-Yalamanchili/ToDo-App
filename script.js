48efab8d00cce6f40ba822b89dffbe5e03d3afa4
Vijayeswar-Yalamanchili <vijayeswar_y@yahoo.com>
2023-12-15 23:45:14 +0530

    updated

--- script.js
+++ script.js
@@ -1,81 +1,97 @@
-const form = document.querySelector("#form");
-const inputTextTitle = document.querySelector("#inputTextTitle");
-const dateInput = document.querySelector("#endDate");
-const textArea = document.querySelector("#textArea");
-const msg = document.querySelector("#msg");
-const tasks = document.querySelector(".tasks")
-const addBtn = document.querySelector("#addBtn")
+const form = document.getElementById("form");
+const textInput = document.getElementById("textInput");
+const msg = document.getElementById("msg");
+const dateInput = document.getElementById("dateInput");
+const textarea = document.getElementById("textarea");
+const tasks = document.getElementById("tasks");
+const add = document.getElementById("add");
 
+//! submit logic
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  formValidation();
+});
 
-//submit form from modal
-form.addEventListener("submit",(e) => {
-    formValidation();
-    e.preventDefault(); 
-}) 
-
-//form-validation for input task title in modal
+//!form-validation function
 const formValidation = () => {
-    if(inputTextTitle.value === ""){
-        msg.innerText = "Input field cannot be empty"
-    }else{
-        msg.innerText = "";
-        getData();
-        //to close modal by clickin add btn by using data-bs-dismiss attribute
-        addBtn.setAttribute("data-bs-dismiss", "modal")     //it takes 2 clicks to close
-        addBtn.click();                                     // to make 1 click by fn call, so on 2nd click it will close
+  if (textInput.value === "") {
+    msg.innerHTML = "Input Field cannot be empty";
+    //console.log("data not found");
+  } else {
+    msg.innerHTML = "";
+    //console.log("data found");
+    getData();
+    add.setAttribute("data-bs-dismiss", "modal");
+    add.click();
+    (() => {
+      add.setAttribute("data-bs-dismiss", "");
+    })();
+  }
+};
 
-        (()=> {
-            addBtn.setAttribute("data-bs-dismiss", "")      // to prevent closing modal without data in input field
-        })()     
-    }
-}
+//!get details from form input and store it in data
+let data = [{}];
 
-//getting data from the form input an store it in data object
-const data = {}
 const getData = () => {
-    // using bracket notation to give datas to object
-    data["text"] = inputTextTitle.value; 
-    data["date"] = dateInput.value;
-    data["task"] = textArea.value;
-    createTodo();
-}
+  data.push({
+    text: textInput.value,
+    date: dateInput.value,
+    task: textarea.value,
+  });
 
-//create todo fn
+  localStorage.setItem("data", JSON.stringify(data));
+  console.log(data);
+  createTodo();
+};
+
+//!create todo function
+
 const createTodo = () => {
-    tasks.innerHTML += `
-    <div class="task">
-        <div class="taskHeader">
-            <span class="fw-bold">${data.text}</span>
-            <span class="fw-light">${data.date}</span>
-        </div>
-        <div class="taskData">
-            <div class="taskText">${data.task}</div>
-            <span class="options">
-                <i onclick = "editTask(this)" data-bs-toggle="modal" data-bs-target="#form" class="fa-regular fa-pen-to-square"></i>
-                <i onclick = "deleteTask(this)" class="fa-regular fa-trash-can"></i>
-            </span>
-        </div>
-    </div>`
-    resetFormData();
-}           //delete & edit task be executed by this keyword in fun. call on icon
+  tasks.innerHTML = "";
+  data.map((ele, y) => {
+    return (tasks.innerHTML += `<div id=${y}>
+    <span class="fw-bolder">${ele.text}</span>
+    <span class="small text-secondary">${ele.date}</span>
+    <p>${ele.task}</p>
+    <span class="options">
+      <i onclick="editData(this)" data-bs-toggle="modal" data-bs-target="#form" class="fa-regular fa-pen-to-square"></i>
+      <i onclick="deleteData(this);createTodo()"class="fa-regular fa-trash-can"></i>
+    </span>
+  </div>`);
+  });
 
-//reset form or clear data
-const resetFormData = () => {
-    inputTextTitle.value = "";
-    dateInput.value = "";
-    textArea.value = "";
-}
+  resetForm();
+};
 
-//delete the task
-const deleteTask = (e) => {
-    e.parentElement.parentElement.parentElement.remove();       //for that delete icon 3 parents are thr in root
-}
+//!reset form to clear the data
 
-//edit the task data
-const editTask = (e) => {
-    let task = e.parentElement.parentElement.parentElement;
-    inputTextTitle.value = task.children[0].children[0].innerHTML;
-    dateInput.value = task.children[0].children[1].innerHTML;
-    textArea.value = task.children[1].children[0].innerHTML;
-    task.remove();
-}
+const resetForm = () => {
+  textInput.value = "";
+  dateInput.value = "";
+  textarea.value = "";
+};
+
+(() => {
+  data = JSON.parse(localStorage.getItem("data")) || [];
+  console.log(data);
+  createTodo();
+})();
+
+//!delete the details from  data
+const deleteData = (e) => {
+  e.parentElement.parentElement.remove();
+  data.splice(e.parentElement.parentElement.id, 1);
+  localStorage.setItem("data", JSON.stringify(data));
+  console.log(data);
+};
+
+//!edit the details in the data
+const editData = (e) => {
+  let task = e.parentElement.parentElement;
+  textInput.value = task.children[0].innerHTML;
+  dateInput.value = task.children[1].innerHTML;
+  textarea.value = task.children[2].innerHTML;
+
+  //task.remove();
+  deleteData(e);
+};
